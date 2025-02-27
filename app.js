7f0b6461ff37d851a91be72d0ca254a2a7a56872
Rj-fredrick Rico <rico.rj-fredrick@student.greenriver.edu>
2025-02-27 13:35:29 -0800

    added app.js, import files, stuff like that

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mariadb from 'mariadb';
+import dotenv from 'dotenv';
+
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static('public'));
+const PORT = 3000;
+app.get('/', (req,res) => {
+    res.render('LOREM');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
