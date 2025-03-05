7d14616c81693f7185536fa1bba415c22a845aed
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-03-05 15:07:23 -0800

    Commented import statement

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mariadb from 'mariadb';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
 const app = express();
 app.use(express.urlencoded({extended:true}));
@@ -8,7 +8,7 @@ app.set('view engine','ejs');
 app.use(express.static('public'));
 const PORT = 3000;
 app.get('/', (req,res) => {
-    res.render('LOREM');
+    res.render('home');
 });
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
