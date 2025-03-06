cd874d342c08567e9772d4018eb138a6373144e9
rjfredr <fredrickalexander1727@gmail.com>
2025-03-06 14:10:40 -0800

    added baclend sql -rj

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 import express from 'express';
 import mariadb from 'mariadb';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+async function connect() {
+    try {
+        const connection = await pool.getConnection();
+        console.log(`Connected to database: ${pool.database}`);
+        return connection;
+    } catch (err) {
+        console.log('Error connecting to database: ' + err);
+    }
+}
 
 const app = express();
 app.use(express.urlencoded({extended:true}));
@@ -10,6 +29,20 @@ const PORT = 3000;
 app.get('/', (req,res) => {
     res.render('home');
 });
+app.get('/addMovie', (req,res) => {
+    res.render('addMovie');
+});
+app.post('/addedMovie', async(req,res) => {
+    const newMovie = {
+        title: req.body.title,
+        director: req.body.director,
+        year: req.body.year
+    }
+    const connection = await connect();
+    const movies = await connection.query(`INSERT INTO movieLog (title,director,year) VALUES ("${newMovie.title}","${newMovie.director}",${newMovie.year});`);
+    console.log(newMovie);
+    res.render('home');
+});
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
---
+++ dbsql.sql
@@ -0,0 +1,12 @@
+-- CREATE DATABASE filmvault;
+USE filmvault;
+SELECT * FROM movieLog;
+-- CREATE TABLE movieLog (
+-- 	movieLogID INT auto_increment,
+--     title VARCHAR(255),
+--     director VARCHAR(255),
+--     year INT(4),
+--     entryDate DATETIME,
+--     
+--     PRIMARY KEY (movieLogID)
+-- );
