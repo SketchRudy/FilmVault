00ecff0d9b27ca80384b4439828308ecbbb46708
RJ <fredrickalexander1727@gmail.com>
2025-03-20 01:40:39 -0700

    add client/server side validation, adding rating feature, touched styles

--- dbsql.sql
+++ dbsql.sql
@@ -1,26 +1,18 @@
 -- CREATE DATABASE filmvault;
 -- USE filmvault;
 
--- validate if
--- username is unique
--- password meets requirements
--- CREATE TABLE users (
--- 	userID INT auto_increment,
---     username VARCHAR(255),
---     password VARCHAR(255),
---     
---     PRIMARY KEY (userID)
--- );
-
--- validate if
--- year is valid
 -- CREATE TABLE movieLog (
 -- 	movielogID INT auto_increment,
 --     title VARCHAR(255),
 --     director VARCHAR(255),
 --     genre VARCHAR(255),
 --     year INT,
+--     rating INT,
 --     comments TEXT,
     
 --     PRIMARY KEY (movielogID)
 -- );
+
+-- DROP TABLE movieLog
+
+-- SELECT * FROM movieLog;
