73d8ba94c07a77f6ea401c79c57ca4d68935d78f
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-03-25 22:09:58 -0700

    Added userID column and logic for non registered users to be unable to add movies

--- dbsql.sql
+++ dbsql.sql
@@ -2,16 +2,14 @@
 -- USE filmvault;
 
 -- CREATE TABLE movieLog (
--- 	movielogID INT auto_increment,
+-- 	   movielogID INT auto_increment,
 --     title VARCHAR(255),
 --     director VARCHAR(255),
 --     genre VARCHAR(255),
 --     year INT,
 --     rating INT,
 --     comments TEXT,
---     userID INT, 
---     PRIMARY KEY (movielogID,
---     FOREIGN KEY (userID) REFERENCES users(userID)
+--     PRIMARY KEY (movielogID)
 -- );
 
 -- DROP TABLE movieLog
